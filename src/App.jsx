efea82b13cf31f12b5775dbba12e9495afaac844
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-07-12 23:39:02 +0600

    tasker app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <HeroSection></HeroSection>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
